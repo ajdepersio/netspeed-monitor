feb7f8bc9ec293842f70030a347207082b9f2fd6
ajdepersio <ajdepersio@gmail.com>
2018-03-08 10:48:32 -0500

    Added date/time to output

--- main.pl
+++ main.pl
@@ -1,26 +1,28 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use DateTime;
 
 my $speedTestResults = `speedtest-cli --simple`;
 
 my $ping = $speedTestResults =~ /Ping:\s(.*)\sms/ ? $1 : 'Error Retrieving Ping';
 my $download = $speedTestResults =~ /Download:\s(.*)\sMbit\/s/ ? $1 : 'Error Retrieving Download Speed';
 my $upload = $speedTestResults =~ /Upload:\s(.*)\sMbit\/s/ ? $1 : 'Error Retrieving Upload Speed';
+my $now = DateTime->now;
 
 my $outFile = 'out.csv';
 if (!(-e $outFile)) 
 {
     #Make the out file if not exist and write the headers as well as the data
     open(FILE, ">$outFile") || die("Error creating $outFile");
-    print FILE "Ping ms,Download Mbit/s,Upload Mbit/s";
-    print FILE "\n$ping,$download,$upload";
+    print FILE "Date/Time,Ping ms,Download Mbit/s,Upload Mbit/s";
+    print FILE "\n$now,$ping,$download,$upload";
     close(FILE);
 }
 else 
 {
     #just write the new data
     open(FILE, ">>$outFile") || die("Error opening $outFile");
-    print FILE "\n$ping,$download,$upload";
+    print FILE "\n$now,$ping,$download,$upload";
     close(FILE);
 }
